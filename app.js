8545065925eeef2e3c33c44ce4533d0eed9cb58c
Ankitpareek-dev <ankitpareek.dev@gmail.com>
2025-06-10 00:46:53 +0530

    added header in the food app

---
+++ app.js
@@ -0,0 +1,37 @@
+// import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://png.pngtree.com/png-vector/20220623/ourmid/pngtree-food-logo-png-image_5296974.png"
+        />
+      </div>
+
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
+
+//creating root
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+//rendering root
+root.render(<AppLayout />);
