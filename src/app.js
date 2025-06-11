d4f8a4abb2b1d0247fd2b81178a6b730b816bcbc
Ankitpareek-dev <ankitpareek.dev@gmail.com>
2025-06-11 19:53:36 +0530

    addig react router

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import About from "./components/About";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
 const AppLayout = () => {
   return (
@@ -12,6 +14,16 @@ const AppLayout = () => {
   );
 };
 
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    Element: <AppLayout></AppLayout>,
+  },
+  {
+    path: "/about",
+    element: <About></About>,
+  },
+]);
 //creating root
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
---
+++ src/components/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+  return (
+    <div>
+      <h1>About page</h1>
+      <h2>Description for about us page</h2>
+    </div>
+  );
+}
+
+export default About;
