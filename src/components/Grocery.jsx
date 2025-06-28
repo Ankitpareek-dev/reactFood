1f745890d5057c3baddf0c05c9b0cb3413030a60
Ankitpareek-dev <ankitpareek.dev@gmail.com>
2025-06-28 16:51:49 +0530

    refactoring the entire frontend

--- src/components/Grocery.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-const Grocery = () => {
-  return <h1>Groceries</h1>;
-};
-
-export default Grocery;
--- src/utils/constants.js
+++
@@ -1,7 +0,0 @@
-export const CND_RESTAURENT_URL = `https://res.cloudinary.com/demo/image/upload/imageId.jpg`;
-
-export const HEADER_LOGO_URL =
-  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIMwNELMuonKbrlFp9XZjomI8TW-kj5RV4DQ&s";
-
-export const RESTAURENT_LIST_URL =
-  "https://mocki.io/v1/691a0f0b-0259-433f-ac9f-03cd56aaf3fa";
